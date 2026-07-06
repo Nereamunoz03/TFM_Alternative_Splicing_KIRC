39ffbfd704c0317f047106ff40197cbb19f1ce8b
Nereamunoz03 <munoznerea03@gmail.com>
2026-07-06 20:34:27 +0200

    Update ANEXO_B_DEGs.R

--- Anexos/ANEXO_B_DEGs.R
+++ Anexos/ANEXO_B_DEGs.R
@@ -2,8 +2,8 @@
 # Trabajo Fin de Máster
 # Máster Universitario en Bioinformática - UNIR
 #
-# Título: Análisis del splicing alternativo asociado a la progresión tumoral
-#          en carcinoma renal de células claras mediante RNA-seq
+# Título: Análisis multiómico de expresión génica y splicing alternativo en 
+#         carcinoma renal de células claras (KIRC).
 #
 # Script: ANEXO_B_DEGs.R
 # Correspondiente al Anexo B del repositorio.
